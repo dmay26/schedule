60cfdb1cbdf48e412d07ce80c9708e79fbb4be88
Dylan May <magic.may26@hotmail.com>
2018-10-10 13:23:16 -0600

    styled color and library structure

--- src/components/scheduleCourse.js
+++ src/components/scheduleCourse.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { connect } from 'react-redux';
-import { toggleEnrolled } from 'react-redux'
+import * as actions from '../actions';
 
+import Action from './action';
 
 class ScheduleCourse extends Component {
-    render() {
-        return (
-            <div className="schedule-course">
-                <label>{this.props.title}</label>
-            
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="schedule-course">
+        <label className="schedule-course__label">{this.props.title}</label>
+        <Action onClick={() => this.props.toggleEnrolled(this.props.id)} className="schedule-course__action"/>
+      </div>
+    );
+  }
 }
 
-export default ScheduleCourse;
+export default connect(null, actions)(ScheduleCourse);
--- src/reducers/coursesReducers.js
+++ src/reducers/coursesReducers.js
@@ -1,4 +1,6 @@
-import { FETCH_COURSES, TOGGLE_ENROLLED } from '../actions/types';
+import { FETCH_COURSES, 
+        TOGGLE_ENROLLED 
+    } from '../actions/types';
 
 export default function(state = [], action) {
     switch (action.type) {
@@ -10,8 +12,17 @@ export default function(state = [], action) {
 
             ]
         case TOGGLE_ENROLLED:
-        
-            return state
+        console.log(action.payload)
+            const newCourses = state.map(course => {
+                if(course.id == action.payload) {
+                    course.enrolled = !course.enrolled
+                }
+                return course
+            })
+            return [
+                ...newCourses
+            ]
+            
         default:
             return state;
     }
