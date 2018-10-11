794f9a5b1647e81ef8642ef6c398d78da5f1b61b
Dylan May <magic.may26@hotmail.com>
2018-10-11 11:04:29 -0600

    animations in course library

--- src/components/action.js
+++ src/components/action.js
@@ -2,13 +2,41 @@ import React, { Component } from 'react';
 import Icon from './icon';
 
 class Action extends Component {
-    render() {
-        return (
-            <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                { Icon('fas fa-plus-circle')}
-            </button>
-        )
+
+    constructor(props) {
+        super(props)
+
+        this.status = false;
     }
-}
+
+    handleAction = function() {
+        this.props.onClick()
+        if(!status) {
+            document.getElementById(this.id).className.add('action-remove')
+        } else {
+            document.getElementById(this.id).className.remove('action-remove')
+        }
+        
+        this.status = !this.status
+        
+    }.bind(this);
+    render() {
+        this.id = `action ${this.props.id}`
+        return (
+            <a
+                id={this.id}
+                onClick={() => this.handleAction()}
+                className={`${this.props.className} action`}>
+            </a>
+        )
+    }    
+}    
+//         return (
+//             <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
+//                 { Icon('fas fa-plus-circle')}
+//             </button>
+//         )
+//     }
+// }
 
 export default Action;
